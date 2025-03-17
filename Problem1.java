14f65258e15178fc6123d91b6e08bdca5abbe3e5
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-03-16 19:13:11 -0500

    Add interview 1 problems - MissingElement and MyMinHeap

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,37 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+// Approach: find mid. we need to check element - index == 1 or not if yes then element is correctly placed in its place, so check left part like that with low and mid places and move to right or left part accordingly. low + 1 will be the missing element.
+
+public class MissingElement {
+
+    public static int getMissingElement(int[] input) {
+        int low = 0, high = input.length - 1;
+//        int missingIndex = -1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+//            if((input[mid] - mid == 2)) {
+//                missingIndex = mid;
+//            }
+            if((input[low] - low == 1) && (input[mid] - mid == 1)) {
+                //left contains all elements so skip this part
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+//        return missingIndex + 1;
+        return low + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] input1 = new int[]{1,2,3,4,6,7};
+        System.out.println("Missing Element in array: " + MissingElement.getMissingElement(input1));
+
+        int[] input2 = new int[]{2,3,4,5,6,7,8,9};
+        System.out.println("Missing Element in array: " + MissingElement.getMissingElement(input2));
+    }
+}
+
 
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,140 @@
+// Time Complexity : getMin = O(1), extractMin() = O(log n), insert() = O(log n)
+// Space Complexity : O(n) array space
+// Did this code successfully run : Yes
+// Any problem you faced while coding this : No
+// Approach: keep static array to store elements. left child index 2*i + 1, right child index = 2*i + 2, parent node index = i-1/2, for get it is simple get root, for extract, remove root and heapify(), for insert at end of array and bubble up using while to find correct place for element.
+
+public class MyMinHeap {
+    private int[] heap;
+    private int size;   //no of elements in heap
+    private int capacity;    //variable to create initial storage array
+
+    public MyMinHeap(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.heap = new int[capacity];
+    }
+
+    public int getMin() {
+        if(this.size == 0) {
+            System.out.println("Heap is empty");
+            return -1;
+        }
+        return heap[0];
+    }
+
+    //function to insert new key in heap -> becomes O(log n) due to bubble up to finalize the correct place for the new key
+    void insert(int key) {
+        if(this.size >= this.capacity) {
+            System.out.println("Heap is full, no more space");
+            return;
+        }
+        this.heap[size] = key;  //store key
+        int currentIndex = size;    //store current index
+        size++; //increment index to put next element and size
+
+        //bubble up
+        while(currentIndex != 0 && heap[getParentIndex(currentIndex)] > heap[currentIndex]) {
+            swap(getParentIndex(currentIndex), currentIndex);   //swap values at parent and current index
+            currentIndex = getParentIndex(currentIndex);    //move up the current index
+        }
+    }
+
+    //helper function to get index of parent of index i
+    private int getParentIndex(int i) {
+        return (i - 1) / 2;
+    }
+
+    //helper function to get index of left child of index i
+    private int getLeftChildIndex(int i) {
+        return (2 * i + 1);
+    }
+
+    //helper function to get index of right child of index i
+    private int getRightChildIndex(int i) {
+        return (2 * i + 2);
+    }
+
+    //helper function to swap values at indices i and j
+
+    private void swap(int i, int j) {
+        int temp = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = temp;
+    }
+
+
+    //function to delete and return min from minHeap -> O(log n) due to call of function heapify()
+    public int extractMin() {
+        if(this.size == 0) {
+            System.out.println("Heap is empty, Nothing to delete");
+            return  -1;
+        }
+        int root = heap[0]; //min value to return as output
+        heap[0] = heap[size - 1];   //copy last value to root for easy deletion
+        this.size--;
+        //bubble down the root to its correct place using heapify()
+        heapify(0);
+
+        return root;
+    }
+
+    //helper function to restore min heap structure bu bubble down root to correc place
+    public void heapify(int i) {
+        int leftChild = getLeftChildIndex(i);
+        int rightChild = getRightChildIndex(i);
+        int smallIndex = i;
+
+        //check if left is smaller
+        if(leftChild < size && heap[leftChild] < heap[smallIndex]) {
+            smallIndex = leftChild;
+        }
+
+        //check if right is smaller
+        if(rightChild < size && heap[rightChild] < heap[smallIndex]) {
+            smallIndex = rightChild;
+        }
+        //if smallIndex is not same as current index i then swap values and call heapify again on small index
+        if(smallIndex != i) {
+            swap(i, smallIndex);
+            heapify(smallIndex);
+        }
+    }
+
+    //function to print heap contents
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The Min Heap is ");
+
+        // Creating object of class in main() method
+        MyMinHeap minHeap = new MyMinHeap(15);
+
+        // Inserting element to minHeap using insert() method
+        // Custom input entries
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+
+        // Print all elements of the heap
+        minHeap.printHeap();
+
+        // Removing minimum value from above heap and printing it
+        System.out.println("Removing the Min val is " + minHeap.extractMin());
+        //Displaying minmium value after extractMin()
+        System.out.println("Current Min val is " + minHeap.getMin());
+    }
+
+}
 
